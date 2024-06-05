877a96435277856b8217ea9bfe5418eb9b3ecbe3
albert.mnac <albert.mnac@gmail.com>
2024-06-06 01:02:56 +0400

    add txt animation in home section

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -1,10 +1,16 @@
 import "./HomeSection.scss"
+import { ReactTyped } from "react-typed";
 
 export default function HomeSection() {
     return (
         <>
             <section className="home flex-center">
-                <h1>front-end developer</h1>
+                <h1>
+                    <ReactTyped
+                        strings={["front-end developer"]}
+                        typeSpeed={100}
+                    />
+                </h1>
             </section>
         </>
     )
