d36a241792dcae9f0b531f515c8480f966a930b4
albert.mnac <albert.mnac@gmail.com>
2024-06-07 15:28:07 +0400

    add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import AboutSection from "./components/AboutSection/AboutSection";
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
 import ContactsSection from "./components/ContactsSection/ContactsSection";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <AboutSection/>
         <ProjectsSection/>
         <ContactsSection/>
+        <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import Container from "../Container/Container";
+
+import "./Footer.scss"
+
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <Container>
+                    <a href="#" className="home_link">al.</a>
+                    <span>coding by <strong>me</strong></span>
+                </Container>
+            </footer>
+        </>
+    )
+}
